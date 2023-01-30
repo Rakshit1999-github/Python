a934a82cbf09344f139598d729346cd6522f0bce
Rakshit1999-github <61899828+Rakshit1999-github@users.noreply.github.com>
2023-01-31 00:10:56 +0530

    web page

---
+++ dom.js
@@ -0,0 +1,6 @@
+var yellow = document.getElementsByClassName('not-same-name');
+yellow[0].textContent = 'classname';
+console.log(yellow);
+var pink = document.getElementsByTagName('li')
+pink[4].textContent = "Tagname"
+console.log(pink)
