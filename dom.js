db17ec67b1f9701869dd2e4ac6558b0244988d81
Rakshit1999-github <61899828+Rakshit1999-github@users.noreply.github.com>
2023-01-31 00:49:06 +0530

    queryselectorAll

--- dom.js
+++ dom.js
@@ -1,6 +1,9 @@
-var yellow = document.getElementsByClassName('not-same-name');
-yellow[0].textContent = 'classname';
-console.log(yellow);
-var pink = document.getElementsByTagName('li')
-pink[4].textContent = "Tagname"
-console.log(pink)
+var xx = document.getElementsByClassName("list-group-item");
+xx[0].style.backgroundColor = 'green';
+xx[1].style.backgroundColor="gray";
+var tt = document.querySelector(".list-group-item:nth-child(2)");
+tt.style.color='green';
+var txt = document.querySelectorAll("li:nth-child(odd)");
+for (var i=0; i<txt.length; i++){
+    txt[i].style.backgroundColor= "green";
+}
